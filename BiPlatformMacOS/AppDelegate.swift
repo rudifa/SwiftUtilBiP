6ab5395eabc3a7c84138c19e003ff95f42b4a75e
rudifa <rudi.farkas@gmail.com>
2019-07-28 10:09:51 +0200

    add target *MacOS

---
+++ BiPlatformMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BiPlatformMacOS
+//
+//  Created by Rudolf Farkas on 28.07.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BiPlatformMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BiPlatformMacOS
+//
+//  Created by Rudolf Farkas on 28.07.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
