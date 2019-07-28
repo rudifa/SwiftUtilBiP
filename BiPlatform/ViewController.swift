70573a7ab93569d1ce24c804c75814022bc44c58
rudifa <rudi.farkas@gmail.com>
2019-07-28 10:05:50 +0200

    Initial Commit

---
+++ BiPlatform/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiPlatform
+//
+//  Created by Rudolf Farkas on 28.07.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
