fd3020276ba909828d3deabcc4016570764a40f1
rudifa <rudi.farkas@gmail.com>
2019-08-06 12:10:53 +0200

    ViewController: display pan and touchClick locations in labels

--- Shared/ViewController.swift
+++ Shared/ViewController.swift
@@ -18,9 +18,62 @@
 // https://www.hackingwithswift.com/example-code/uikit/how-to-create-auto-layout-constraints-in-code-constraintswithvisualformat
 // https://stackoverflow.com/questions/26180822/how-to-add-constraints-programmatically-using-swift
 
-
 class ViewController: NSUIViewController {
     var lastPanLocation = CGPoint()
+
+    lazy var label1: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.yellow
+        label.text = "Pan from"
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var label2: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.yellow
+        label.text = ""
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var label3: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.yellow
+        label.text = "to"
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var label4: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.yellow
+        label.text = ""
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var label5: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.orange
+        label.text = "TapClick at"
+        label.sizeToFit()
+        return label
+    }()
+
+    lazy var label6: NSUILabel = {
+        let label = NSUILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = NSUIColor.orange
+        label.text = ""
+        label.sizeToFit()
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,41 +84,6 @@ class ViewController: NSUIViewController {
     }
 
     fileprivate func addLabels() {
-        let label1 = NSUILabel()
-        label1.translatesAutoresizingMaskIntoConstraints = false
-        label1.backgroundColor = NSUIColor.red
-        label1.text = "THESE"
-        label1.sizeToFit()
-
-        let label2 = NSUILabel()
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        label2.backgroundColor = NSUIColor.cyan
-        label2.text = "ARE"
-        label2.sizeToFit()
-
-        let label3 = NSUILabel()
-        label3.translatesAutoresizingMaskIntoConstraints = false
-        label3.backgroundColor = NSUIColor.yellow
-        label3.text = "SOME"
-        label3.sizeToFit()
-
-        let label4 = NSUILabel()
-        label4.translatesAutoresizingMaskIntoConstraints = false
-        label4.backgroundColor = NSUIColor.green
-        label4.text = "AWESOME"
-        label4.sizeToFit()
-
-        let label5 = NSUILabel()
-        label5.translatesAutoresizingMaskIntoConstraints = false
-        label5.backgroundColor = NSUIColor.orange
-        label5.text = "LABELS"
-        label5.sizeToFit()
-
-        let label6 = NSUILabel()
-        label6.translatesAutoresizingMaskIntoConstraints = false
-//        label6.backgroundColor = NSUIColor.orange
-        label6.text = "LABELS"
-        label6.sizeToFit()
 
         view.addSubview(label1)
         view.addSubview(label2)
@@ -74,34 +92,18 @@ class ViewController: NSUIViewController {
         view.addSubview(label5)
         view.addSubview(label6)
 
-        /* Example
-         view.addConstraintsWithFormat(format: "H:|-8-[v0(100)]-8-[v1(>=20)]-8-[v2(100)]-8-|", views: previousButton, pageControl, nextButton)
-         */
-        //    let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:[view]-(<=0)-[newView(100)]", options: NSLayoutConstraint.FormatOptions.alignAllCenterY, metrics: nil, views: views)
-        //    let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[view]-(<=0)-[newView(100)]", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: views)
 
-
-        view.addConstraintsWithFormat(format: "H:|-50-[v0(100)]-8-[v1(100)]", views: label1, label2)
+        view.addConstraintsWithFormat(format: "H:|-50-[v0(150)]-8-[v1(150)]", views: label1, label2)
         view.addConstraintsWithFormat(format: "V:|-100-[v0]", views: label1)
         view.addConstraintsWithFormat(format: "V:|-100-[v0]", views: label2)
 
-        view.addConstraintsWithFormat(format: "H:|-50-[v0(100)]-8-[v1(100)]", views: label3, label4)
+        view.addConstraintsWithFormat(format: "H:|-50-[v0(150)]-8-[v1(150)]", views: label3, label4)
         view.addConstraintsWithFormat(format: "V:|-150-[v0]", views: label3)
         view.addConstraintsWithFormat(format: "V:|-150-[v0]", views: label4)
 
-        view.addConstraintsWithFormat(format: "H:|-50-[v0(100)]-8-[v1(100)]", views: label5, label6)
-        view.addConstraintsWithFormat(format: "V:|-200-[v0]", views: label5)
-        view.addConstraintsWithFormat(format: "V:|-200-[v0]", views: label6)
-
-
-
-//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-//
-//        for label in viewsDictionary.keys {
-//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
-//        }
-
-//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-50-[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraintsWithFormat(format: "H:|-50-[v0(150)]-8-[v1(150)]", views: label5, label6)
+        view.addConstraintsWithFormat(format: "V:|-250-[v0]", views: label5)
+        view.addConstraintsWithFormat(format: "V:|-250-[v0]", views: label6)
     }
 }
 
@@ -132,7 +134,7 @@ extension ViewController: NSUIGestureRecognizerDelegate {
     @objc func handleTapClick(recognizer: NSUITapClickGestureRecognizer) {
         let location = recognizer.locationFromTop(in: view)
         printClassAndFunc(info: "\(location.fmt)")
-        // works on macOS and on iOS device, not on iOS simulator
+        label6.text = "\(location.fmt)"
     }
 
     @objc func handlePan(recognizer: NSUIPanGestureRecognizer) {
@@ -143,11 +145,13 @@ extension ViewController: NSUIGestureRecognizerDelegate {
         switch recognizer.state {
         case .began:
             lastPanLocation = location
+            label2.text = "\(lastPanLocation.fmt)"
         case .changed:
             _ = Float((lastPanLocation.x - location.x) / view.bounds.width) * panSensitivity
             _ = Float((lastPanLocation.y - location.y) / view.bounds.height) * panSensitivity
             // printClassAndFunc(info: "\(xDelta) \(yDelta) ")
             lastPanLocation = location
+            label4.text = "\(lastPanLocation.fmt)"
         case .ended:
             break
         default:
