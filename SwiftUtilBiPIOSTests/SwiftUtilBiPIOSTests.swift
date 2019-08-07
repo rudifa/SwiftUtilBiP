9cf4cd14a3c30f8df73420971b046c9e0e4911eb
rudifa <rudi.farkas@gmail.com>
2019-08-07 16:53:01 +0200

    add iOS and macOS unit test targets

---
+++ SwiftUtilBiPIOSTests/SwiftUtilBiPIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUtilBiPIOSTests.swift
+//  SwiftUtilBiPIOSTests
+//
+//  Created by Rudolf Farkas on 07.08.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import XCTest
+
+class SwiftUtilBiPIOSTests: XCTestCase {
+    override func setUp() {}
+
+    override func tearDown() {}
+
+    func testExample() {}
+}
---
+++ SwiftUtilBiPMacOSTests/SwiftUtilBiPMacOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUtilBiPMacOSTests.swift
+//  SwiftUtilBiPMacOSTests
+//
+//  Created by Rudolf Farkas on 07.08.19.
+//  Copyright © 2019 Rudolf Farkas. All rights reserved.
+//
+
+import XCTest
+
+class SwiftUtilBiPMacOSTests: XCTestCase {
+    override func setUp() {}
+
+    override func tearDown() {}
+
+    func testExample() {}
+}
